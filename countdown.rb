c2c795677c23555d2bd1197b78eb8aada34f5d35
osama miro <o.miro650@gmail.com>
2017-12-16 04:47:12 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  counter = num
+  while num > 0
+    puts "#{num} SECONDS!"
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 end
